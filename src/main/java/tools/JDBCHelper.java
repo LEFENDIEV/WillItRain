4334d799d7253db4078168a4ed915aeda5605844
lefendiev <leon.efendiev@gmail.com>
2019-11-07 14:58:26 +0200

    FIX
    - Removed default value null for connection
    - Removed condition for connection

--- src/main/java/tools/JDBCHelper.java
+++ src/main/java/tools/JDBCHelper.java
@@ -12,7 +12,7 @@ import java.sql.*;
  *
  * */
 public class JDBCHelper {
-    private Connection connection = null;
+    private Connection connection;
     private String url;
     private String username;
     private String password;
@@ -39,9 +39,7 @@ public class JDBCHelper {
      * */
     public Connection connect(){
         try {
-            if(connection.isClosed()){
-                this.connection = DriverManager.getConnection(this.url, this.username, this.password);
-            }
+            this.connection = DriverManager.getConnection(this.url, this.username, this.password);
         }catch (Exception e){
             System.out.println("Connection "+e);
         }finally {
