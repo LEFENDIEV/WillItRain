d1b0c7d6b5e750a6edbe5747e0380265407d72d7
lefendiev <leon.efendiev@gmail.com>
2019-11-26 14:11:57 +0200

    FEAT - REFACTOR formatted_adress attribute
    - Added the formatted_adress to location table
    - Added the change to the  insert request
    - Changed the value for the salt and password for the user insert request

--- sql/dump.sql
+++ sql/dump.sql
@@ -14,20 +14,21 @@ CREATE TABLE location (
     id int NOT NULL,
     longitude FLOAT,
     latitude FLOAT,
+    formatted_adress VARCHAR(255),
     id_user int NOT NULL,
     FOREIGN KEY (id_user) REFERENCES users(id),
     PRIMARY KEY (id_user, id)
 );
 
--- helloimpassword
-INSERT INTO users(login, password, salt) values ("michel", "Jzb8qmD1Go0x0rzh28Lv6Q==", "4459682Fw8FcXWxRug3e0qzktQhGIwKX6jgEPXXlvXhGsbQwsdEW8UhHX2rhVpH4gTiCx+lPGtV+He1MlD/OgC8i2+mwFRqSahbJ6aCtYDMiaIFgXCpg7xMlBaPoqa7c2eFPBH1qjBsUsqISXGueHGSVuTKTJaKDYNF24p0BtaA="); 
--- helloagain
-INSERT INTO users(login, password, salt) values ("boris", "cPeRtBAUqkAqX49j0o8HMA==", "yeIsSBs1MVyiAApUCOu1HBKmlqAv8li3v6KLZiCREM9MrVM8UXf31VvrfBiaLUWkYw+8NpOaVpoeRi7TbiSkCsCkkVUaPH8o11PO14ZpUU673XnuV975FOCKRKp1NstZpzlvjlqM2JtgAzTS6grqcRpV3MMSVPs8IePRn8nVuWI=");
+-- michel 
+INSERT INTO users(login, password, salt) values ("michel", "9gRPTCPqQPydcZEq9WZckQ==", "tvb0yyQdkEssW02LCSBMSVw3PiFXPheL8H0d5UfrUtlokz4A9GOYAoRRUd2QGPaiiwNe+TUMIW80abBbFDVb3OVAuNChkVo6Im3oY6jbUt+TfFvstINsXrekJ3DFPehiSB/UPCBloZvjDH1hgaAszXJilBJKVy6iX1ylg/pGaBY="); 
+-- borisss
+INSERT INTO users(login, password, salt) values ("boris", "VMD/YzA71YUug4C1vt2TIA==", "IUobPjGUiwwj9pVczxd173vajAZmcgLqsvcpNWoV9TvQpyweK10EjR0f0dRH0Cl2m/N8wXLXIbhlT1nxEFeXvrizHogkhQCrR73+pi4cyw467oUIHPcKa1eQmwXWmDwLEmEqvIzEG3e1mY/QiGzQDG5TBwfAGM+h1ampTDhsoZk=");
 
 
-INSERT INTO location(id, longitude, latitude, id_user) VALUES (1, 45, 24, 1);
-INSERT INTO location(id, longitude, latitude, id_user) VALUES (2, 24, 45, 1);
-INSERT INTO location(id, longitude, latitude, id_user) VALUES (3, 75, 89, 1);
-INSERT INTO location(id, longitude, latitude, id_user) VALUES (1, 45, 24, 2);
-INSERT INTO location(id, longitude, latitude, id_user) VALUES (2, 65, 123, 2);
+INSERT INTO location(id, longitude, latitude, formatted_adress, id_user) VALUES (1, 42.145424, 24.758526, "fsdjflsdf6456q", 1);
+INSERT INTO location(id, longitude, latitude, formatted_adress, id_user) VALUES (2, 42.138829, 24.768332,"6465sd1fsd1f6 sdf sdf 4651", 1);
+INSERT INTO location(id, longitude, latitude, formatted_adress, id_user) VALUES (3, 44.205375, 17.904493,"fdsfs dfsdf sdf sdf sdf", 1);
+INSERT INTO location(id, longitude, latitude, formatted_adress, id_user) VALUES (1, 40.814869, -73.127136," fsdf  ", 2);
+INSERT INTO location(id, longitude, latitude, formatted_adress, id_user) VALUES (2, 10.501565, 107.171710,"test qdress" , 2);
 
