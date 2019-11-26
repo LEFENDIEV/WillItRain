19a0de285f744fc43b1fb64f040057c95a2d63e5
lefendiev <leon.efendiev@gmail.com>
2019-11-26 16:58:46 +0200

    FEAT after middleware / default return
    - Added middleware which will send status 401 if nothing happened (finalMessage still empty)
    - added response status 401 with exception throws

--- src/main/java/WillItRain.java
+++ src/main/java/WillItRain.java
@@ -63,6 +63,11 @@ public class WillItRain {
                 response.header("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,");
                 response.header("Access-Control-Allow-Credentials", "true");
             });
+            after(((request, response) -> {
+                if(request.body().length()==0){
+                    response.status(401);
+                }
+            }));
             //Return the user with the specified id
             get(endpointList.get("returnUserById"), (request, response) -> {
                 String dbReturn = "";
@@ -81,15 +86,16 @@ public class WillItRain {
                         response.status(301);
                     }
                 }catch (Exception e){
+                    response.status(401);
                     System.out.println("getUser"+e);
                 }finally {
-                    return "return get = " + dbReturn;
+                    return  dbReturn;
                 }
             });
             //Allow the user to create a token for his session and redirect to front page
             post(endpointList.get("loging"), ((request, response) -> {
                 String token;
-                String finalMessage = "loging";
+                String finalMessage = "";
                 try{
                     if(request.cookie("user-token")!=null){
                         token = request.cookie("user-token");
@@ -124,14 +130,16 @@ public class WillItRain {
                         }
                     }
                 }catch (Exception e){
+                    response.status(401);
                     System.out.println("loging"+e);
+                }finally {
+                    return finalMessage;
                 }
-                return finalMessage;
             }));
             //Create an user with the data of POST
             post(endpointList.get("registration"), (request , response) -> {
                 String token = "";
-                String finalMessage = "registration";
+                String finalMessage = "";
                 try {
                     response.type("application/json");
                     User user = new Gson().fromJson(request.body(), User.class);
@@ -173,6 +181,7 @@ public class WillItRain {
             });
             //Delete the user with the specified id
             delete(endpointList.get("deleteUserById"), (request , response) -> {
+                String finalMessage = "";
                 try {
                     if(request.cookie("user-token")!=null){
                         String token = request.cookie("user-token");
@@ -180,6 +189,7 @@ public class WillItRain {
                         User user = userDAO.getUserByLogin(request.cookie("login"));
                         if(sparkJWTHelper.isWillItRainTokenValid(token, salt,  user)){
                             userDAO.deleteUser(Integer.parseInt(request.params(":id")));
+                            finalMessage = "user deleted";
                             response.status(200);
                         }else{
                             response.status(401);
@@ -188,13 +198,15 @@ public class WillItRain {
                         response.status(401);
                     }
                 }catch (Exception e){
+                    response.status(401);
                     System.out.println("deleteUser"+e);
                 }finally {
-                    return "Delete user";
+                    return finalMessage;
                 }
             });
             //Modify the user with the specified id using POST data
             put(endpointList.get("modifyUserById"), (request , response) -> {
+                String finalMessage = "";
                 try {
                     if(request.cookie("user-token")!=null){
                         String token = request.cookie("user-token");
@@ -205,6 +217,7 @@ public class WillItRain {
                             Type typeListAttr     = new TypeToken<List<String>>(){}.getType();
                             List<String> listAttr = gson.fromJson(request.body(), typeListAttr);
                             userDAO.editUser(Integer.parseInt(request.params(":id")), listAttr.get(0), listAttr.get(1), listAttr.get(2));
+                            finalMessage="user modfied";
                             response.status(200);
                         }else{
                             response.status(401);
@@ -213,16 +226,17 @@ public class WillItRain {
                         response.status(401);
                     }
                 }catch (Exception e){
+                    response.status(401);
                     System.out.println("updateUser"+e);
                 }finally {
-                    return "Update user";
+                    return finalMessage;
                 }
             });
             //Return if it is raining in the specified location
             get(endpointList.get("getWeatherByLocation"), (request, response) -> {
                 URL url        = new URL("https://api.you-weather.com/get/data/en?c="+request.params(":location"));
                 InputStream is = url.openStream();
-                String result  = "";
+                String finalMessage  = "";
                 try {
                     if(request.cookie("user-token")!=null){
                         String token = request.cookie("user-token");
@@ -235,9 +249,9 @@ public class WillItRain {
                             JSONObject obj      = new JSONObject(jsonContent);
                             JSONObject location = obj.getJSONObject("data").getJSONArray("nearest_area").getJSONObject(0);
 
-                            result += "You are in "+location.getJSONArray("areaName").getJSONObject(0).getString("value");
-                            result += " located in "+location.getJSONArray("country").getJSONObject(0).getString("value");
-                            result += " and today it is "+obj.getJSONObject("data").getJSONArray("current_condition").getJSONObject(0).getJSONArray("weatherDesc").getJSONObject(0).getString("value")+" !";
+                            finalMessage += "You are in "+location.getJSONArray("areaName").getJSONObject(0).getString("value");
+                            finalMessage += " located in "+location.getJSONArray("country").getJSONObject(0).getString("value");
+                            finalMessage += " and today it is "+obj.getJSONObject("data").getJSONArray("current_condition").getJSONObject(0).getJSONArray("weatherDesc").getJSONObject(0).getString("value")+" !";
                             response.status(200);
                         }else{
                             response.status(401);
@@ -246,14 +260,16 @@ public class WillItRain {
                         response.status(401);
                     }
                 } catch (Exception e) {
+                    response.status(401);
                     System.out.println("willItRainLocation" + e);
                 }
-                return result;
+                return finalMessage;
             });
             //Return if it is raining in the specified city using a redirection of the previous endpoint
             get(endpointList.get("getWeatherByCity"), (request, response) -> {
                 URL url        = new URL("https://api.you-weather.com/get/suggest/en?q="+request.params(":city"));
                 InputStream is = url.openStream();
+                String finalMessage="";
                 try {
                     if(request.cookie("user-token")!=null){
                         StringWriter writer = new StringWriter();
@@ -265,7 +281,7 @@ public class WillItRain {
                             String jsonContent  = writer.toString();
                             JSONObject obj      = new JSONObject(jsonContent);
                             String location     = obj.getJSONArray("data").getString(0);
-
+                            finalMessage = "redirection";
                             redirect.get("/user/:id/willitrain/"+location, "/user/:id/willitrain/:city");
                         }else{
                             response.status(401);
@@ -274,12 +290,14 @@ public class WillItRain {
                         response.status(401);
                     }
                 } catch (Exception e) {
+                    response.status(401);
                     System.out.println("willItRainCity" + e);
                 }
-                return "redirection";
+                return finalMessage;
             });
             //Redirect to the front page where the map is displayed
             get(endpointList.get("getFrontPageMap"), ((request, response) ->{
+                String finalMessage="";
                 try {
                     if(request.cookie("user-token")!=null){
                         String token = request.cookie("user-token");
@@ -294,6 +312,7 @@ public class WillItRain {
                             response.type("application/json");
                             response.body(json);
                             response.status(200);
+                            finalMessage = json;
                         }else{
                             response.status(401);
                         }
@@ -301,14 +320,16 @@ public class WillItRain {
                         response.status(401);
                     }
                 }catch(Exception e){
+                    response.status(401);
                     System.out.println("getFrontPageMap" + e);
                 }
                 finally {
-                    return "frontpage";
+                    return finalMessage;
                 }
             } ));
             //Redirect to the front page where the map is displayed
             post(endpointList.get("setLocation"), ((request, response) ->{
+                String finalMessage="";
                 try {
                     if(request.cookie("user-token")!=null){
                         String token = request.cookie("user-token");
@@ -324,6 +345,7 @@ public class WillItRain {
                             Location currentLocation = new Location(incrementalId, Float.valueOf(listAttr.get(0)), Float.valueOf(listAttr.get(1)), user, listAttr.get(2));
                             System.out.println(currentLocation.toString());
                             locationDAO.addLocation(currentLocation);
+                            finalMessage = "location set";
                             response.status(200);
                         }else{
                             response.status(401);
@@ -332,10 +354,11 @@ public class WillItRain {
                         response.status(401);
                     }
                 }catch(Exception e){
+                    response.status(401);
                     System.out.println("getFrontPageMap" + e);
                 }
                 finally {
-                    return "frontpage";
+                    return finalMessage;
                 }
             } ));
         }catch(Exception e){
